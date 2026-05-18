12644bb0690e96fb811c503806b260f0f4619bf9
Nicolai <245527909+predictor2718@users.noreply.github.com>
2026-05-18 10:09:25 +0200

    feat: add GL046 rule for cache key derived from user-controlled variable

--- rules/owasp.go
+++ rules/owasp.go
@@ -48,6 +48,7 @@ var owaspCategories = map[string][]string{
 	"GL043": {"CICD-SEC-5"},
 	"GL044": {"CICD-SEC-4"},
 	"GL045": {"CICD-SEC-9"},
+	"GL046": {"CICD-SEC-3"},
 }
 
 // owaspCategoryNames maps OWASP CI/CD Security Risks category IDs to their names.
